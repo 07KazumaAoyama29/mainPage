485ff1932e5f31031205e544c7ac64cddb4812bf
kazuma <bloodtune65@gmail.com>
2025-10-21 21:13:34 +0900

    update

--- roulette_app/models.py
+++ roulette_app/models.py
@@ -7,9 +7,18 @@ class LabMember(models.Model):
         ('COMM', '通信'),
         ('GRAPH', 'グラフ'),
     ]
+    YEAR_CHOICES = [
+        ('B4', '学部4年'),
+        ('M1', '修士1年'),
+        ('M2', '修士2年'),
+        ('D', '博士課程'),
+        ('PD', '研究員'),
+        ('STAFF', '教員'),
+    ]
 
     name = models.CharField("メンバー名", max_length=100)
     research_group = models.CharField("所属グループ", max_length=10, choices=GROUP_CHOICES)
+    academic_year = models.CharField("学年", max_length=10, choices=YEAR_CHOICES, default='M1') 
     is_active = models.BooleanField("在籍中", default=True) # 卒業生などを非表示にするため
 
     class Meta:
