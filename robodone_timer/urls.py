61ebd11c2e47a86e60830b01cab4bd1bd2fd508e
kazuma <bloodtune65@gmail.com>
2025-12-09 23:04:57 +0900

    update robodone's timer

--- robodone_timer/urls.py
+++ robodone_timer/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'robot_timer'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.menu, name='menu'),             # トップページ
+    path('build/', views.build_timer, name='build_timer'), # 制作タイマー
+    path('pair/', views.pair_timer, name='pair_timer'),   # 交代タイマー
 ]
--- robodone_timer/views.py
+++ robodone_timer/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'robodone_timer/index.html')
+def menu(request):
+    return render(request, 'robodone_timer/menu.html') # 作成したメニュー画面
+
+def build_timer(request):
+    return render(request, 'robodone_timer/build_timer.html') # ロボット制作タイマー
+
+def pair_timer(request):
+    return render(request, 'robodone_timer/index.html') # 交代タイマー（index.htmlの場合）
